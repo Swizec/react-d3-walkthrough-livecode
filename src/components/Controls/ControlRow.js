b58981f2fd69ee34dbde52efd4917dc731fd22a4
Swizec Teller <swizec@swizec.com>
2018-10-28 17:26:10 -0700

    Render ControlRow for years

---
+++ src/components/Controls/ControlRow.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Toggle from "./Toggle";
+
+class ControlRow extends React.Component {
+    makePick = (picked, newState) => {
+        this.props.updateDataFilter(picked, !newState);
+    };
+
+    _addToggle(name) {
+        let key = `toggle-${name}`,
+            label = name;
+
+        if (this.props.capitalize) {
+            label = label.toUpperCase();
+        }
+
+        return (
+            <Toggle
+                label={label}
+                name={name}
+                key={key}
+                value={this.props.picked === name}
+                onClick={this.makePick}
+            />
+        );
+    }
+
+    render() {
+        const { toggleNames } = this.props;
+
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    {toggleNames.map(name => this._addToggle(name))}
+                </div>
+            </div>
+        );
+    }
+}
+export default ControlRow;
--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -26,10 +26,34 @@ class Controls extends React.Component {
         );
     };
 
+    reportUpdateUpTheChain() {
+        window.location.hash = [this.state.year || "*"].join("-");
+
+        this.props.updateDataFilter(
+            (filters => {
+                return d => filters.yearFilter(d);
+            })(this.state),
+            {
+                year: this.state.year
+            }
+        );
+    }
+
     render() {
         const { data } = this.props;
 
-        return null;
+        const years = new Set(data.map(d => d.submit_date.getFullYear()));
+
+        return (
+            <div>
+                <ControlRow
+                    data={data}
+                    toggleNames={Array.from(years.values())}
+                    picked={this.state.year}
+                    updateDataFilter={this.updateYearFilter}
+                />
+            </div>
+        );
     }
 }
 
