7a8efb30304990943c5a52f57d1e533f7b495e2d
Swizec Teller <swizec@swizec.com>
2019-03-02 16:48:54 -0800

    Refactor axis to useD3

--- src/components/Histogram/Axis.js
+++ src/components/Histogram/Axis.js
@@ -1,14 +1,19 @@
 import * as d3 from "d3";
-import D3blackbox from "../D3blackbox";
+import React from "react";
+import { useD3 } from "d3blackbox";
 
-const Axis = D3blackbox(function() {
-    const axis = d3
-        .axisLeft()
-        .tickFormat(d => `${d3.format(".2s")(d)}`)
-        .scale(this.props.scale)
-        .ticks(this.props.data.length);
+function Axis({ x, y, scale, data }) {
+    const anchorRef = useD3(function(anchorRef) {
+        const axis = d3
+            .axisLeft()
+            .tickFormat(d => `${d3.format(".2s")(d)}`)
+            .scale(scale)
+            .ticks(data.length);
 
-    d3.select(this.anchorRef.current).call(axis);
-});
+        d3.select(anchorRef).call(axis);
+    });
+
+    return <g transform={`translate(${x}, ${y})`} ref={anchorRef} />;
+}
 
 export default Axis;
